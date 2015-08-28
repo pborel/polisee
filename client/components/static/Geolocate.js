8acc9aa011aef71029b2d0d43b147b6fe196f7f3
Michal Wong <contact.michaelwong@gmail.com>
2015-08-28 11:30:48 -0700

    add Geolocate js for clientside execution

---
+++ client/components/static/Geolocate.js
@@ -0,0 +1,8 @@
+function getLocation() {
+        navigator.geolocation.getCurrentPosition(setGeoCookie);
+    }
+
+function setGeoCookie(position) {
+  var cookie_val = position.coords.latitude + "|" + position.coords.longitude;
+  document.cookie = "lat_lng=" + escape(cookie_val);
+}
