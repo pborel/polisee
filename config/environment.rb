1d9dd092d21cb20131aea2b1945e13714fadf118
Ryan Posthumus <ryanposthumus19@gmail.com>
2015-08-29 14:59:20 -0700

    add seed file for all legislators

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+Rails.application.config.autoload_paths << Rails.root.join('lib')
 # Initialize the Rails application.
 Rails.application.initialize!
+
 
