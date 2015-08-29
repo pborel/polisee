872077fe9884c8847cf8a2730f9c989fda1dc614
Ryan Posthumus <ryanposthumus19@gmail.com>
2015-08-28 19:31:10 -0700

    add Legislator table populate w/ sunlight and add twitter images

--- app/controllers/legislators_controller.rb
+++ app/controllers/legislators_controller.rb
@@ -1,17 +1,39 @@
 class LegislatorsController < ApplicationController
   include Sun
 
+
   def index
-    client = Congress.new
-    reps_search = client.local_politicians(94105)
+    sun_client = Congress.new
+    reps_search = sun_client.local_politicians(94105)
     @local_politicians = JSON.parse(reps_search.body)
-    p @local_politicians
-    bill_search = client.bills
+    bill_search = sun_client.bills
     @all_bills = JSON.parse(bill_search.body)
-    p @all_bills
+
+    getTwitterImages(@local_politicians)
+    twitter_client = generate_twitter_client
+
+    p user.profile_image_url_https
+
   end
 
   def show
+
+  end
+
+private
+  def getTwitterImages(@local_politicians)
+    twitter_client = generate_twitter_client
+    get_image_for_each(@local_politicians)
+  end
+
+  def generate_twitter_client
+    Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+      config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+    end
+  end
+
+  def get_image_for_each(@local_politicians)
 
   end
 
